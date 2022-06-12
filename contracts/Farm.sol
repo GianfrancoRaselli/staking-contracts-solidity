126ea071eafef985211dd3419449c8a9c18e36c6
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2022-06-12 10:26:27 -0300

    first commit

---
+++ contracts/Farm.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "./StakingToken.sol";
+import "./RewardToken.sol";
+
+
+contract Farm {
+  
+  address public owner = msg.sender;
+
+  StakingToken public stakingToken;
+  RewardToken public rewardToken;
+
+  address[] public stakers;
+  mapping(address => uint) stakingBalance;
+  mapping(address => bool) hasStaked;
+  mapping(address => bool) isStaking;
+
+
+  modifier onlyOwner {
+    require(msg.sender == owner, "You can not access");
+    _;
+  }
+
+
+  constructor(StakingToken _stakingToken, RewardToken _rewardToken) {
+    stakingToken = _stakingToken;
+    rewardToken = _rewardToken;
+  }
+
+
+  function stakeTokens(uint _amount) public {
+    require(_amount > 0, "You can not stake 0 tokens");
+
+    stakingToken.transferFrom(msg.sender, address(this), _amount);
+    stakingBalance[msg.sender] += _amount;
+    if (!hasStaked[msg.sender]) {
+      stakers.push(msg.sender);
+      hasStaked[msg.sender] = true;
+    }
+    isStaking[msg.sender] = true;
+  }
+
+  function unstakeTokens() public {
+    uint balance = stakingBalance[msg.sender];
+    require(balance > 0, "You do not have staked tokens");
+
+    stakingToken.transfer(msg.sender, balance);
+    delete stakingBalance[msg.sender];
+    delete isStaking[msg.sender];
+  }
+
+  function unstakeTokens(uint _amount) public {
+    uint balance = stakingBalance[msg.sender];
+    require(balance >= _amount, "Try to unstake fewer tokens");
+
+    stakingToken.transfer(msg.sender, _amount);
+    unchecked {
+      stakingBalance[msg.sender] = balance - _amount;
+    }
+    if (balance == _amount) {
+      delete isStaking[msg.sender];
+    }
+  }
+
+  function issueTokens() public onlyOwner {
+    for (uint i = 0; i < stakers.length;) {
+      address recipient = stakers[i];
+      uint balance = stakingBalance[recipient];
+
+      if (balance > 0) {
+        rewardToken.transfer(recipient, balance);
+      }
+
+      unchecked {
+        i++;
+      }
+    }
+  }
+
+}
---
+++ contracts/RewardToken.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+
+contract RewardToken {
+
+  string public name = "Reward Token";
+  string public symbol = "RT";
+  uint public totalSupply = 100000000000; // 1.000.000 tokens
+  uint8 public decimals = 5;
+
+  mapping(address => uint) public balanceOf;
+  mapping(address => mapping(address => uint)) public allowance;
+
+
+  event Transfer(
+    address indexed from,
+    address indexed to,
+    uint value
+  );
+
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint value
+  );
+
+
+  constructor() {
+    balanceOf[msg.sender] = totalSupply;
+  }
+  
+
+  function transfer(address _to, uint _value) public returns (bool success) {
+    require(balanceOf[msg.sender] >= _value);
+
+    unchecked {
+      balanceOf[msg.sender] -= _value; 
+    }
+    balanceOf[_to] += _value;
+
+    emit Transfer(msg.sender, _to, _value);
+
+    return true;
+  }
+
+  function approve(address _spender, uint _value) public returns (bool success) {
+    allowance[msg.sender][_spender] = _value;
+
+    emit Approval(msg.sender, _spender, _value);
+
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
+    require(balanceOf[_from] >= _value);
+    require(allowance[_from][msg.sender] >= _value);
+
+    unchecked {
+      balanceOf[_from] -= _value; 
+      allowance[_from][msg.sender] -= _value;
+    }
+    balanceOf[_to] += _value;
+
+    emit Transfer(_from, _to, _value);
+
+    return true;
+  }
+
+}
